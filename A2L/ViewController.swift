089045b63644aa31fa428d76012eea8da7f4fc7f
Nathan Stchepinsky <nathanstchepinsky@gmail.com>
2019-01-08 22:30:18 +0100

    Initial Commit

---
+++ A2L/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A2L
+//
+//  Created by Nathan on 08/01/2019.
+//  Copyright © 2019 Nathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ A2LTests/A2LTests.swift
@@ -0,0 +1,34 @@
+//
+//  A2LTests.swift
+//  A2LTests
+//
+//  Created by Nathan on 08/01/2019.
+//  Copyright © 2019 Nathan. All rights reserved.
+//
+
+import XCTest
+@testable import A2L
+
+class A2LTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
